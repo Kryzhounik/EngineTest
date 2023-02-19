499f62810493e15b71c6360884f2776bc0944b62
Kryzhounik <ilya.dokuchaew@gmail.com>
2023-02-19 17:14:37 +0200

    switch to async behavior

--- FileEngine/src/main/java/org/red/fileEngine/client/AppEntryPoint.java
+++ FileEngine/src/main/java/org/red/fileEngine/client/AppEntryPoint.java
@@ -7,8 +7,8 @@ public class AppEntryPoint {
 		System.out.println("starting...");
 		
 		//could be added some other way to switch between options
-		ConsoleClient client = new SyncClient();
-		//ConsoleClient client = new AsyncClient();
+		//ConsoleClient client = new SyncClient();
+		ConsoleClient client = new AsyncClient();
 		client.run();
 		System.out.println("stoped");
 	}
